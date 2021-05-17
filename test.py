d6401ad2605eef1e5a977ea67c2097cabe5e13b2
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-17 14:58:14 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,35 @@
+from scipy.stats import norm
+
+print(norm.logpdf(0.0))
+print(norm.logpdf([0.0]))
+print(norm.logpdf([[0.0]]))
+print(norm.logpdf(0.0, loc=5))
+print(norm.logpdf(0.0, loc=[5]))
+## -0.918938533205
+## [-0.91893853]
+## [[-0.91893853]]
+## -13.4189385332
+## [-13.41893853]
+
+from edward.stats import norm
+import tensorflow as tf
+sess = tf.InteractiveSession()
+
+print(norm.logpdf(0.0).eval())
+print(norm.logpdf([0.0]).eval())
+print(norm.logpdf([[0.0]]).eval())
+print(norm.logpdf(0.0, loc=5).eval())
+print(norm.logpdf(0.0, loc=[5]).eval())
+## edward with this pull request:
+## -0.918939
+## [-0.91893852]
+## [[-0.91893852]]
+## -13.4189
+## [-13.41893864]
+##
+## edward without this pull request:
+## -0.918939
+## -0.918939
+## -0.918939
+## -13.4189
+## -13.4189
