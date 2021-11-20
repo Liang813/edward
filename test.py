e9fc908cdf009bf030af8c9987af50b7e328c667
lyk <62755766+Liang813@users.noreply.github.com>
2021-11-20 19:15:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,39 @@
 from scipy.stats import norm
+import traceback
+try:
+  print(norm.logpdf(0.0))
+  print(norm.logpdf([0.0]))
+  print(norm.logpdf([[0.0]]))
+  print(norm.logpdf(0.0, loc=5))
+  print(norm.logpdf(0.0, loc=[5]))
+  ## -0.918938533205
+  ## [-0.91893853]
+  ## [[-0.91893853]]
+  ## -13.4189385332
+  ## [-13.41893853]
 
-print(norm.logpdf(0.0))
-print(norm.logpdf([0.0]))
-print(norm.logpdf([[0.0]]))
-print(norm.logpdf(0.0, loc=5))
-print(norm.logpdf(0.0, loc=[5]))
-## -0.918938533205
-## [-0.91893853]
-## [[-0.91893853]]
-## -13.4189385332
-## [-13.41893853]
+  from edward.stats import norm
+  import tensorflow as tf
+  sess = tf.InteractiveSession()
 
-from edward.stats import norm
-import tensorflow as tf
-sess = tf.InteractiveSession()
-
-print(norm.logpdf(0.0).eval())
-print(norm.logpdf([0.0]).eval())
-print(norm.logpdf([[0.0]]).eval())
-assert(norm.logpdf([[0.0]]).eval().shape == (1,1))
-print(norm.logpdf(0.0, loc=5).eval())
-print(norm.logpdf(0.0, loc=[5]).eval())
-## edward with this pull request:
-## -0.918939
-## [-0.91893852]
-## [[-0.91893852]]
-## -13.4189
-## [-13.41893864]
-##
-## edward without this pull request:
-## -0.918939
-## -0.918939
-## -0.918939
-## -13.4189
-## -13.4189
+  print(norm.logpdf(0.0).eval())
+  print(norm.logpdf([0.0]).eval())
+  print(norm.logpdf([[0.0]]).eval())
+  assert(norm.logpdf([[0.0]]).eval().shape == (1,1))
+  print(norm.logpdf(0.0, loc=5).eval())
+  print(norm.logpdf(0.0, loc=[5]).eval())
+  ## edward with this pull request:
+  ## -0.918939
+  ## [-0.91893852]
+  ## [[-0.91893852]]
+  ## -13.4189
+  ## [-13.41893864]
+  ##
+  ## edward without this pull request:
+  ## -0.918939
+  ## -0.918939
+  ## -0.918939
+  ## -13.4189
+  ## -13.4189
+except Exception as e:
+  traceback.print_exc(file=open('/script/edward46-buggy.txt','w+'))
